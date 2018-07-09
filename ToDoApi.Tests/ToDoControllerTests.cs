77637bbbaccdc84300b0a23b5aa67e45875cc02c
MikolHutnik <mikolhutnik@gmail.com>
2018-07-09 10:20:30 -0400

    first commit

---
+++ ToDoApi.Tests/ToDoControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToDoApi.Tests
+{[TestFixture]
+    public class ToDoControllerTests
+    {
+        [Test]
+        public void Test()
+        {
+            Assert.Fail();
+        }
+    }
+}
