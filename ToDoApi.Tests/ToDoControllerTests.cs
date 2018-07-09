854fc28f5922c34318bad24aeb3e467c0eff4a30
MikolHutnik <mikolhutnik@gmail.com>
2018-07-09 13:10:04 -0400

    ToDoApi0002

--- ToDoApi.Tests/ToDoControllerTests.cs
+++ ToDoApi.Tests/ToDoControllerTests.cs
@@ -4,15 +4,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using ToDoApi.Controllers;
+using ToDoApi.Models;
 
 namespace ToDoApi.Tests
-{[TestFixture]
+{
+    [TestFixture]
     public class ToDoControllerTests
     {
         [Test]
-        public void Test()
+        public void GetAll_Returns_List_Of_Todos()
         {
-            Assert.Fail();
+            var controller = new TodoController();
+            var result = controller.GetAll();
+            Assert.That(result.Count, Is.EqualTo(1));
+
+        }
+        [Test]
+        public void Post_Saves_Todo()
+        {
+            var todo = new Todo();
+            var controller = new TodoController();
+
+            controller.Post(todo);
+            var result = controller.GetAll();
+            Assert.That(result.Count, Is.EqualTo(2));
         }
     }
 }
---
+++ ToDoApi/Controllers/TodoController.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using ToDoApi.Models;
+
+namespace ToDoApi.Controllers
+{
+    public class TodoController : ApiController
+    {
+        private static List<Todo> todos = new List<Todo>()
+        {
+             {
+                new Todo() {Id = 1, Name = "Feed cat", Priority = 1 }
+            };
+    }
+        public List<Todo> GetAll()
+        {
+            return new List<Todo>()
+            {
+                new Todo() {Id = 1, Name = "Feed cat", Priority = 1 }
+            };
+        }
+
+        public void Post(Todo todo)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
